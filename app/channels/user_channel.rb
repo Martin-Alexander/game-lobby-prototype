59a0fcf67001ca5efbb677e4b3fca8493351ef56
Martin-Alexander <martingianna@gmail.com>
2017-09-09 04:13:22 -0400

    completed simple 'who's online' tracker

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -1,10 +1,10 @@
 class UserChannel < ApplicationCable::Channel
   def subscribed
     stream_from "user_channel"
-    puts "subscribed: #{current_user.username}" if current_user
+    current_user.update(online: true) if current_user
   end
 
   def unsubscribed
-    puts "unsubscribed: #{current_user.username}" if current_user
+    current_user.update(online: false) if current_user
   end  
 end

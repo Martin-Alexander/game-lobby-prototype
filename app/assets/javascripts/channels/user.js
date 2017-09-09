a0ae3173117b06c0cda6a6cd9e6b63768eb72cf8
Martin Giannakopoulos <martingianna@gmail.com>
2017-09-09 05:54:07 -0400

    number of users online updates automatically across websocket

--- app/assets/javascripts/channels/user.js
+++ app/assets/javascripts/channels/user.js
@@ -1,5 +1,7 @@
 App.game = App.cable.subscriptions.create("UserChannel", {
   connected: function() {},
   disconnected: function() {},
-  received: function() {}
+  received: function(data) {
+    $("#number-of-users-online").text(data.number_of_users_online);
+  }
 });
--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -2,9 +2,15 @@ class UserChannel < ApplicationCable::Channel
   def subscribed
     stream_from "user_channel"
     current_user.update(online: true) if current_user
+    update_number_of_users_online
   end
 
   def unsubscribed
     current_user.update(online: false) if current_user
-  end  
+    update_number_of_users_online
+  end
+
+  def update_number_of_users_online
+    ActionCable.server.broadcast "user_channel", {number_of_users_online: User.how_many_online}
+  end
 end

36ad1f1f25612d2f55a8836fc3d36bbf4fab0485
Martin-Alexander <martingianna@gmail.com>
2017-09-08 23:00:48 -0400

    inital commit with devise-by-username setup

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable, :authentication_keys => [:username]
+
+  validates :username, uniqueness: true, length: { minimum: 6 }
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+end

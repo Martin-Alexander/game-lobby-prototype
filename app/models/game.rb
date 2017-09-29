f7617fb0c8e45360381faa7390b0fab8fcb9810e
Martin-Alexander <martingianna@gmail.com>
2017-09-29 17:07:34 -0400

    added leave/join/create functionality for users and lobbies

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,17 @@ class Game < ApplicationRecord
   has_many :users, through: :players
 
   validates :state, inclusion: { in: ["lobby", "game_on", "game_over", "paused"] }
+
+  def user_in_game?(player_object)
+    check_if_user(player_object)
+    self.players.where(user_id: player_object.id).any?
+  end
+
+  private
+
+  def check_if_user(object)
+    unless object.is_a?(User)
+      raise ArgumentError, "Invalid user: #{object}"
+    end  
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,12 +22,53 @@ class User < ApplicationRecord
   end
 
   def is_in
-    if Game.joins(:players).where("players.user_id = ? AND in_game = ?", self.id, true).any?
+    if Game.joins(:players).where("players.user_id = ? AND players.in_game = ?", self.id, true).any?
       "game"
     elsif Game.joins(:players).where("players.user_id = ? AND state = ?", self.id, "lobby").any?
       "lobby"
     else
       "menus"
     end
+  end
+
+  def lobby
+    Game.joins(:players).where("players.user_id = ? AND state = ?", self.id, "lobby").first
+  end
+
+  def join_lobby(game)
+    check_if_lobby(game)
+    if !game.user_in_game?(self)
+      Player.create! user: self, game: game, host: false, role: "player", in_game: false
+    else
+      raise StandardError, "User is already in lobby"
+    end
+  end
+
+  def leave_lobby
+    if self.is_in == "lobby"
+      game = self.lobby
+      Player.where(user: self, game: game).first.destroy!
+      game.destroy! if game.players.count.zero?
+    else
+      raise StandardError, "User is not in lobby"
+    end
+  end
+
+  def create_new_lobby
+    if self.is_in == "menus"
+      new_game = Game.create! data: "", state: "lobby"
+      Player.create! user: self, game: new_game, role: "player", host: true, in_game: false
+      return new_game
+    else
+      raise StandardError, "User must be in 'menus' to create lobby"
+    end
+  end
+
+  private
+
+  def check_if_lobby(object)
+    unless object.is_a?(Game) && object.state == "lobby"
+      raise ArgumentError, "Invalid game: #{object}"
+    end    
   end
 end

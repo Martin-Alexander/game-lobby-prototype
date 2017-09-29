33483663c2008f418372ab863e685ace097930ed
Martin-Alexander <martingianna@gmail.com>
2017-09-29 19:19:06 -0400

    new seed for developing lobby-related views and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,40 +2,50 @@ Player.destroy_all
 Game.destroy_all
 User.destroy_all
 
-lobby_game = Game.create data: "", state: "lobby"
-active_game = Game.create data: "", state: "game_on"
-paused_game = Game.create data: "", state: "paused"
-finished_game = Game.create data: "", state: "game_over"
-
 $default_password = "123456"
 
 def create_user(username, online) 
   User.create username: username, password: $default_password, online: online
 end
 
-martin = create_user("martin", true)
-sophie = create_user("sophie", true)
+martin = create_user("martin", false)
+sophie = create_user("sophie", false)
 chloe = create_user("chloe", false)
-adele = create_user("adele", true)
-brittany = create_user("brittany", true)
+adele = create_user("adele", false)
+brittany = create_user("brittany", false)
 
-# The lobby game
-Player.create user: martin, game: lobby_game, role: "player", host: true, in_game: false
-Player.create user: sophie, game: lobby_game, role: "player", host: false, in_game: false
+chloe_game = Game.create! data: "", state: "lobby"
+adele_game = Game.create! data: "", state: "lobby"
+brittany_game = Game.create! data: "", state: "lobby"
 
-# The active game
-Player.create user: adele, game: active_game, role: "player", host: true, in_game: true
-Player.create user: brittany, game: active_game, role: "player", host: false, in_game: true
+Player.create! user: chloe, game: chloe_game, host: true, role: "player", in_game: false
+Player.create! user: adele, game: adele_game, host: true, role: "player", in_game: false
+Player.create! user: brittany, game: brittany_game, host: true, role: "player", in_game: false
 
-# The paused game
-Player.create user: sophie, game: paused_game, role: "player", host: true, in_game: false
-Player.create user: chloe, game: paused_game, role: "player", host: false, in_game: false
-Player.create user: adele, game: paused_game, role: "player", host: false, in_game: false
-Player.create user: brittany, game: paused_game, role: "player", host: false, in_game: false
+# Join/Create Lobby Test Seed
 
-# The finished game
-Player.create user: martin, game: finished_game, role: "player", host: true, in_game: false
-Player.create user: sophie, game: finished_game, role: "player", host: false, in_game: false
-Player.create user: chloe, game: finished_game, role: "player", host: false, in_game: false
-Player.create user: adele, game: finished_game, role: "player", host: false, in_game: false
-Player.create user: brittany, game: finished_game, role: "player", host: false, in_game: false
+# lobby_game = Game.create data: "", state: "lobby"
+# active_game = Game.create data: "", state: "game_on"
+# paused_game = Game.create data: "", state: "paused"
+# finished_game = Game.create data: "", state: "game_over"
+
+# # The lobby game
+# Player.create user: martin, game: lobby_game, role: "player", host: true, in_game: false
+# Player.create user: sophie, game: lobby_game, role: "player", host: false, in_game: false
+
+# # The active game
+# Player.create user: adele, game: active_game, role: "player", host: true, in_game: true
+# Player.create user: brittany, game: active_game, role: "player", host: false, in_game: true
+
+# # The paused game
+# Player.create user: sophie, game: paused_game, role: "player", host: true, in_game: false
+# Player.create user: chloe, game: paused_game, role: "player", host: false, in_game: false
+# Player.create user: adele, game: paused_game, role: "player", host: false, in_game: false
+# Player.create user: brittany, game: paused_game, role: "player", host: false, in_game: false
+
+# # The finished game
+# Player.create user: martin, game: finished_game, role: "player", host: true, in_game: false
+# Player.create user: sophie, game: finished_game, role: "player", host: false, in_game: false
+# Player.create user: chloe, game: finished_game, role: "player", host: false, in_game: false
+# Player.create user: adele, game: finished_game, role: "player", host: false, in_game: false
+# Player.create user: brittany, game: finished_game, role: "player", host: false, in_game: false

1aa72e4c090b374451050387a6260eae49d7b847
Martin-Alexander <martingianna@gmail.com>
2017-09-29 17:08:37 -0400

    delegated user properties to players

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,12 @@ class Player < ApplicationRecord
   belongs_to :game
 
   validates :role, inclusion: { in: ["player", "dead_player", "observer"] }
+
+  def online
+    self.user.online
+  end
+
+  def username
+    self.user.username
+  end
 end
